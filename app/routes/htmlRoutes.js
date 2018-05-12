711a6f5ed7de4e2eea1ea1336d28dd0aca2b5027
Rodriguez <Eddie.Rodriguez@cpk.jmhuber.com>
2018-05-12 09:59:26 -0400

    updated default path

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -13,4 +13,8 @@
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+
+    app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
   };
